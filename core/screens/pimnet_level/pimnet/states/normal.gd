c2624cdc530268993f8f41df055ece0feecf3b13
svetogam <sviatoslavsokolov@proton.me>
2025-02-18 14:25:59 -0800

    Fix bug breaking pimnet modes when moved between too quickly

--- core/screens/pimnet_level/pimnet/states/normal.gd
+++ core/screens/pimnet_level/pimnet/states/normal.gd
@@ -10,6 +10,11 @@
 
 extends State
 
+func _enter(_last_state: String) -> void:
+	%SystemButton.pressed.connect(_on_system_button_pressed)
+	%PlanButton.pressed.connect(_on_plan_button_pressed)
+	%EditPanelsButton.pressed.connect(_on_edit_panels_button_pressed)
+
 
 func _on_system_button_pressed() -> void:
 	_change_state("System")
@@ -21,3 +26,9 @@ func _on_plan_button_pressed() -> void:
 
 func _on_edit_panels_button_pressed() -> void:
 	_change_state("EditPanels")
+
+
+func _exit(_next_state: String) -> void:
+	%SystemButton.pressed.disconnect(_on_system_button_pressed)
+	%PlanButton.pressed.disconnect(_on_plan_button_pressed)
+	%EditPanelsButton.pressed.disconnect(_on_edit_panels_button_pressed)
