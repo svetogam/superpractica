e85067ac7f48981ad76d6c758e2b4f80eacf9279
svetogam <sviatoslavsokolov@proton.me>
2025-03-04 07:35:54 -0800

    Redesign conditions panel to have 1-3 conditions and not overflow in layout

--- core/screens/pimnet_level/pimnet/overlay/goal_panels/construct_conditions_panel.gd
+++ core/screens/pimnet_level/pimnet/overlay/goal_panels/construct_conditions_panel.gd
@@ -14,8 +14,9 @@ signal verification_requested
 
 var used_condition_rows: int = 0
 @onready var verification_panel := %SolutionVerificationPanel as PanelContainer
-@onready var condition_icons: Array = [%ConditionIcon1, %ConditionIcon2]
-@onready var condition_slots: Array = [%ConditionSlot1, %ConditionSlot2]
+@onready var condition_rows: Array = [%ConditionRow1, %ConditionRow2, %ConditionRow3]
+@onready var condition_icons: Array = [%ConditionIcon1, %ConditionIcon2, %ConditionIcon3]
+@onready var condition_slots: Array = [%ConditionSlot1, %ConditionSlot2, %ConditionSlot3]
 @onready var verify_button := %VerifyConditionsButton as Button
 
 
@@ -30,6 +31,7 @@ func _on_verify_button_pressed() -> void:
 func add_condition(memo: Memo, icon: Texture2D) -> void:
 	condition_icons[used_condition_rows].texture = icon
 	condition_slots[used_condition_rows].set_by_memo(memo, true)
+	condition_rows[used_condition_rows].visible = true
 	verification_panel.correct_memos.append(memo)
 
 	used_condition_rows += 1
