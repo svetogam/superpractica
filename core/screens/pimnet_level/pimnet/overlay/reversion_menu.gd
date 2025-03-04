ed0301832fbff4ee7447ba747330517e62a42b97
svetogam <sviatoslavsokolov@proton.me>
2025-03-04 06:02:16 -0800

    Fix reversion menu layout to follow new design

--- core/screens/pimnet_level/pimnet/overlay/reversion_menu.gd
+++ core/screens/pimnet_level/pimnet/overlay/reversion_menu.gd
@@ -8,7 +8,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later                                 #
 #============================================================================#
 
-extends PanelContainer
+extends Control
 
 signal undo_pressed
 signal redo_pressed
