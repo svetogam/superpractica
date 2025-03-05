10cc38262705943a2065dc8eeaf0a89c2ad887b3
svetogam <sviatoslavsokolov@proton.me>
2025-03-05 03:05:54 -0800

    Redesign verification panel to have 1-4 rows

--- core/screens/pimnet_level/pimnet/overlay/goal_panels/solution_verification_panel.gd
+++ core/screens/pimnet_level/pimnet/overlay/goal_panels/solution_verification_panel.gd
@@ -10,18 +10,16 @@
 
 extends PanelContainer
 
-var row_numbers: Array:
-	get:
-		return range(left_slots.size())
 var verified_row_numbers: Array = []
 var rejected_row_numbers: Array = []
 var filled_row_numbers: Array:
 	get:
 		return verified_row_numbers + rejected_row_numbers
 var correct_memos: Array
-@onready var left_slots: Array = [%LeftSlot1, %LeftSlot2]
-@onready var right_slots: Array = [%RightSlot1, %RightSlot2]
-@onready var check_slots: Array = [%CheckSlot1, %CheckSlot2]
+@onready var rows: Array = [%CheckRow1, %CheckRow2, %CheckRow3, %CheckRow4]
+@onready var left_slots: Array = [%LeftSlot1, %LeftSlot2, %LeftSlot3, %LeftSlot4]
+@onready var right_slots: Array = [%RightSlot1, %RightSlot2, %RightSlot3, %RightSlot4]
+@onready var check_slots: Array = [%CheckSlot1, %CheckSlot2, %CheckSlot3, %CheckSlot4]
 
 
 func _ready() -> void:
@@ -34,13 +32,16 @@ func _ready() -> void:
 func open() -> void:
 	clear_slots()
 	show()
-	for row_number in row_numbers:
+	for row_number in range(correct_memos.size()):
 		left_slots[row_number].set_by_memo(correct_memos[row_number], true)
+		rows[row_number].show()
 
 
 func close() -> void:
 	clear_slots()
 	hide()
+	for row in rows:
+		row.hide()
 
 
 func affirm_in_row(memo: Memo, row_number: int) -> void:
