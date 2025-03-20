81cacf50229067d69c569a910d470f520844637d
svetogam <sviatoslavsokolov@proton.me>
2025-03-19 23:24:16 -0700

    Center number sprites and shrink separation

--- core/systems/info_signals/effects/number_effect.gd
+++ core/systems/info_signals/effects/number_effect.gd
@@ -13,14 +13,19 @@ var number: int:
 
 
 func _update_sprites() -> void:
+	# Set sprites
 	var number_digits := IntegerMath.get_number_of_digits(number)
-
 	for i in range(_digit_sprites.size()):
 		if i <= number_digits - 1:
 			var digit := IntegerMath.get_digit_at_place(number, i + 1)
 			_digit_sprites[i].animation = str(digit)
 		else:
 			_digit_sprites[i].animation = "empty"
+
+	# Center sprites
+	var offset_x = -(number_digits - 1) * (%Tens.position.x / 2) / %Tens.scale.x
+	for sprite in _digit_sprites:
+		sprite.offset.x = offset_x
 
 
 func set_by_effect(original: ScreenEffect) -> void:
