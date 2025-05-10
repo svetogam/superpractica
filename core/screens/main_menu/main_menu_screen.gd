f8cadd9c46575456b74272a0d2e40fc418da4433
svetogam <sviatoslavsokolov@proton.me>
2025-05-09 19:39:47 -0700

    Show version near title

--- core/screens/main_menu/main_menu_screen.gd
+++ core/screens/main_menu/main_menu_screen.gd
@@ -14,18 +14,21 @@ func _ready() -> void:
 	%WebsiteLink.uri = Game.WEBSITE_URL
 	%SourceLink.text = Game.REPO_URL
 	%SourceLink.uri = Game.REPO_URL
+	var version_text: String
 	if YourBuil.has_git_tag():
-		%VersionText.text = YourBuil.git_tag
+		version_text = YourBuil.git_tag
 		%SourceCommitLink.text = ("This Build: " + YourBuil.git_tag
 				+ " [" + YourBuil.get_git_commit_hash(8) + "]")
 		%SourceCommitLink.uri = Game.REPO_URL + "/src/tag/" + YourBuil.git_tag
 	elif YourBuil.has_data():
-		%VersionText.text = Game.version_tag + "+" + str(YourBuil.git_commit_count)
+		version_text = Game.version_tag + "+" + str(YourBuil.git_commit_count)
 		%SourceCommitLink.text = "This Build: [" + YourBuil.get_git_commit_hash(8) + "]"
 		%SourceCommitLink.uri = Game.REPO_URL + "/src/commit/" + YourBuil.git_commit_hash
 	else:
-		%VersionText.text = Game.version_tag + "+"
+		version_text = Game.version_tag + "+"
 		%SourceCommitLink.text = "Local Build"
+	%VersionText.text = version_text
+	%TitleVersion.text = version_text + " Demo"
 	%EngineLicense.text = Engine.get_license_text()
 
 	$StateMachine.activate()
