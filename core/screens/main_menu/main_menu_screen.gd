9f1699d9fae1de8db6956bfaf333529de5894fc8
svetogam <sviatoslavsokolov@proton.me>
2025-07-25 23:54:44 -0700

    Refactor main menu states with state chart

--- core/screens/main_menu/main_menu_screen.gd
+++ core/screens/main_menu/main_menu_screen.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 	if OS.get_name() == "Web":
 		%QuitButton.hide()
 
-	# Present Data
+	# Present info
 	%WebsiteLink.text = Game.WEBSITE_URL
 	%WebsiteLink.uri = Game.WEBSITE_URL
 	%SourceLink.text = Game.REPO_URL
@@ -29,4 +29,96 @@ func _ready() -> void:
 	%TitleVersion.text = version_text + " Demo"
 	%EngineLicense.text = Engine.get_license_text()
 
-	$StateMachine.activate()
+	$StateChart.set_expression_property("transition_duration", %TransitionCamera.duration)
+
+
+func _connect_buttons() -> void:
+	%TopicsButton.pressed.connect(_on_topics_button_pressed)
+	%InfoButton.pressed.connect(_on_info_button_pressed)
+	%QuitButton.pressed.connect(_on_quit_button_pressed)
+	%InfoBackButton.pressed.connect(_on_info_back_button_pressed)
+	%CreditsButton.pressed.connect(_on_credits_button_pressed)
+	%LicensesButton.pressed.connect(_on_licenses_button_pressed)
+	%CreditsBackButton.pressed.connect(_on_credits_back_button_pressed)
+	%LicensesBackButton.pressed.connect(_on_licenses_back_button_pressed)
+	%LicensesBackButton2.pressed.connect(_on_licenses_back_button_pressed)
+	%GameLicenseButton.pressed.connect(_on_game_licenses_button_pressed)
+	%EngineLicenseButton.pressed.connect(_on_engine_licenses_button_pressed)
+
+
+func _disconnect_buttons() -> void:
+	%TopicsButton.pressed.disconnect(_on_topics_button_pressed)
+	%InfoButton.pressed.disconnect(_on_info_button_pressed)
+	%QuitButton.pressed.disconnect(_on_quit_button_pressed)
+	%InfoBackButton.pressed.disconnect(_on_info_back_button_pressed)
+	%CreditsButton.pressed.disconnect(_on_credits_button_pressed)
+	%LicensesButton.pressed.disconnect(_on_licenses_button_pressed)
+	%CreditsBackButton.pressed.disconnect(_on_credits_back_button_pressed)
+	%LicensesBackButton.pressed.disconnect(_on_licenses_back_button_pressed)
+	%LicensesBackButton2.pressed.disconnect(_on_licenses_back_button_pressed)
+	%GameLicenseButton.pressed.disconnect(_on_game_licenses_button_pressed)
+	%EngineLicenseButton.pressed.disconnect(_on_engine_licenses_button_pressed)
+
+
+func _on_camera_viewing_state_entered() -> void:
+	_connect_buttons()
+
+
+func _on_camera_viewing_state_exited() -> void:
+	_disconnect_buttons()
+
+
+func _on_page_child_state_exited() -> void:
+	$StateChart.send_event("move_camera")
+
+
+func _on_licenses_page_child_state_exited() -> void:
+	$StateChart.send_event("move_camera")
+
+
+func _on_topics_button_pressed() -> void:
+	Game.request_enter_level_select.emit()
+
+
+func _on_quit_button_pressed() -> void:
+	Game.request_exit_game.emit()
+
+
+func _on_info_button_pressed() -> void:
+	$StateChart.send_event("view_info")
+	%TransitionCamera.transition_to(%InfoCamera)
+
+
+func _on_info_back_button_pressed() -> void:
+	$StateChart.send_event("view_start")
+	%TransitionCamera.transition_to(%StartCamera)
+
+
+func _on_credits_button_pressed() -> void:
+	$StateChart.send_event("view_credits")
+	%TransitionCamera.transition_to(%CreditsCamera)
+
+
+func _on_licenses_button_pressed() -> void:
+	$StateChart.send_event("view_licenses")
+	%TransitionCamera.transition_to(%GameLicensesCamera)
+
+
+func _on_credits_back_button_pressed() -> void:
+	$StateChart.send_event("view_info")
+	%TransitionCamera.transition_to(%InfoCamera)
+
+
+func _on_licenses_back_button_pressed() -> void:
+	$StateChart.send_event("view_info")
+	%TransitionCamera.transition_to(%InfoCamera)
+
+
+func _on_game_licenses_button_pressed() -> void:
+	$StateChart.send_event("view_game_licenses")
+	%TransitionCamera.transition_to(%GameLicensesCamera)
+
+
+func _on_engine_licenses_button_pressed() -> void:
+	$StateChart.send_event("view_engine_licenses")
+	%TransitionCamera.transition_to(%EngineLicensesCamera)
